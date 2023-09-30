f988b2ea07489e5047b028ddc09985a6ed79e014
Pogba-Ceesay <115332681+Pogba-Ceesay@users.noreply.github.com>
2023-09-30 03:36:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The-solar-system
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-vsfhlj)
---
+++ script.js
@@ -0,0 +1,9 @@
+const intro = document.querySelector(".intro");
+const container = document.querySelector(".container");
+const para = document.querySelector("p");
+
+function showContainer(){
+    container.style.display = "block";
+    intro.style.display = "none";
+    para.style.display = "block";
+}
